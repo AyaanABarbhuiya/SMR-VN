61fa6b832fd74f2a5ddc1afff3f9eed06a5d0cf8
Intel Tinkerer <ayaan.a.barbhuiya@proton.me>
2026-04-10 21:44:09 +0530

    Added typewriter and save functionality to js

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -5,16 +5,33 @@ let sceneData;
 async function initGame() {
     const response= await fetch (dialogues.json);
     sceneData= await response.json();
-    scene= sceneData["start"];
+    const savedScene = localStorage.getItem('current'); // TOBE: REPLACED BY PG
+    if (savedScene && sceneData[savedScene]){
+      scene = sceneData[savedScene];}
+      else { scene = sceneData["start"];}
+   
 } 
 async function render() {
+    await initGame(); 
     document.getElementById("txt").innerHTML = scene.txt;
     document.body.style.backgroundImage = `url(${scene.bg})`;
     document.getElementById("c1").src = scene.c1; 
     document.getElementById("c2").src = scene.c2;
-    
+     // Typewriter Engine
+    var i = 0; 
+    var speed = 100; 
+    function typeWriter (){
+    if (i < scene.txt.length) {
+    document.getElementById("txt").innerHTML += scene.txt.charAt(i);
+    i++;
+    setInterval (typeWriter,speed); 
+     }
+  } 
+  typeWriter ();
 }
-function renderChoices() {
+
+async function renderChoices() {
+    await render(); 
     const choicesContainer = document.getElementById("choices");
     choicesContainer.innerHTML = ""; 
     
@@ -30,8 +47,14 @@ function renderChoices() {
 }
 async function update(next) {
   scene = sceneData[next]; 
+  localStorage.setItem('current', next); // TOBE: REPLACED BY PG
   await render(); 
  }
 
+
+
  /*TODO: Add typewriter effect for text*/
+
+
+
  /*TODO: Temporarily save in localStorage and later to PostgreSQL using pg in Node.js */ 
